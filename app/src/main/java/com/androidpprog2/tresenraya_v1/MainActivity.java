0eaf9ecc89f5cb7497cbe4f2a5eac7a73bf0ac04
Oscar Romero Doblas <oscar.romero@students.salle.url.edu>
2022-02-26 21:19:47 +0100

    Color Primary GREEN (DONE)

--- app/src/main/java/com/androidpprog2/tresenraya_v1/MainActivity.java
+++ app/src/main/java/com/androidpprog2/tresenraya_v1/MainActivity.java
@@ -4,14 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
 
     TextView Jugador1;
     TextView Jugador2;
-
+    Integer[] botones;
+    int[] tablero = new int[]{
+            0, 0, 0,
+            0, 0, 0,
+            0, 0, 0
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +32,32 @@ public class MainActivity extends AppCompatActivity {
         //Escondemos texto
         Jugador2.setVisibility(View.INVISIBLE);
 
+        botones = new Integer[]{
+                R.id.b1, R.id.b2, R.id.b3,
+                R.id.b4, R.id.b5, R.id.b6,
+                R.id.b7, R.id.b8, R.id.b9,
+        };
     }
+
+
+    public void insertarFicha(View v){
+        //Turno jugador 1
+        int numBoton = Arrays.asList(botones).indexOf(v.getId());
+        v.setBackgroundResource(R.drawable.cruz);
+        tablero[numBoton] = 1;
+        //Turno jugador 2
+
+
+    }
+
+/*
+        public void insertarFichaJ2(){
+        Jugador1.setVisibility(View.INVISIBLE);
+        Jugador2.setVisibility(View.VISIBLE);
+       int pos = Arrays.asList(botones).indexOf(v.getId());
+        Button b = (Button) findViewById(botones[pos]);
+        b.setBackgroundResource(R.drawable.circulo);
+        tablero[pos] = 2;
+    }
+*/
 }
